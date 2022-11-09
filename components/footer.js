7becf5076876a214bdfd9685f60d3657f1689278
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2022-11-09 20:32:23 +0530

    day-2

---
+++ components/footer.js
@@ -0,0 +1,90 @@
+const footer=()=>{
+    return `<div id="footer">
+    <div id="bigbasket">
+        <h2>bigbasket</h2>
+        <p>About Us</p>
+        <p>In News</p>
+        <p>Green bigbasket</p>
+        <p>Privacy Policy</p>
+        <p>Affiliate</p>
+        <p>Terms and Conditions</p>
+        <p>Careers At bigbasket</p>
+        <p>bb Instant</p>
+        <p>bb Daily</p>
+        <p>bb Blog</p>
+        <p>bbnow</p>
+    </div>
+    <div id="Help">
+        <h2>Help</h2>
+       <p>FAQs</p>
+       <p>Contact Us</p>
+       <p>bb Wallet FAQs</p>
+       <p>bb Wallet T&Cs</p>
+       <p>Vendor Connect</p>
+ </div>
+ <div id="Download Our App">
+    <h2>Download Our App</h2>
+    <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Google-App-store-icon.png" alt="">
+    <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Apple-App-store-icon.png" alt="">
+ </div>
+ <div id="Get Social With Us">
+    <h2>Get Social With Us</h2>
+    <img src="./facebook-icon.png" alt="">
+    <img src="./pintrest-icon.png" alt="">
+    <img src="./twitter-icon.png" alt="">
+    <img src="./instagram-icon.png" alt="">
+ </div>
+</div>
+<hr style="width:90% ;margin:auto; margin-top: 10px;"/>
+<div id="footer-down">
+   <div>
+    <div class="left">
+        <p>POPULAR CATEGORIES:</p>
+    </div>
+   <div class="right">
+    <p>
+        Sunflower Oils, Wheat Atta, Ghee, Milk, Health Drinks, Flakes, Organic F&V, Namkeen, Eggs, Floor Cleaners, Other Juices, Leafy Vegetables, Frozen Veg Food, Diapers & Wipes,</p>
+   </div>
+   </div>
+   
+   <div>
+    <div class="left">
+        <p>POPULAR BRANDS</p>
+    </div>
+    <div class="right">
+        <p>
+            Amul, Nescafe , MTR, RED BULL , elite cake, Pediasure, Yummiez, Yera, Yakult, Britannia, Wow Momo, Fortune , Haldirams , Ferrero, Lays, Patanjali, McCain, kwality walls, Cadbury Dairy Milk, Pedigree,</p>
+    </div>
+    
+   </div>
+   <div>
+    <div class="left">
+        <p>CITIES WE SERVE:</p>
+    </div>
+   
+    <div class="right">
+        <p>
+            Bangalore, Hyderabad, Mumbai, Pune, Chennai, Delhi, Mysore, Madurai, Coimbatore, Vijayawada-Guntur, Kolkata, Ahmedabad-Gandhinagar, Nashik Business, Lucknow-Kanpur, Gurgaon, Vadodara, Visakhapatnam, Surat, Nagpur, Patna, Indore, Chandigarh Tricity, Jaipur, Bhopal, Noida-Ghaziabad, Kochi, Krishna District, Bhubaneshwar-Cuttack, Guwahati, Renigunta, Hubli, Davanagere, Trichy, Amravati, Raipur, Rajkot, Gwalior, Bareilly, Allahabad, Hyderabad Rural, Bangalore Rural, Chennai Rural, Vizag Rural, Lucknow Rural, Noida Rural, Ahmedabad Rural, Bhopal Rural, Bhubaneswar Rural, Coimbatore Rural, Chandigarh Rural, Gurugram Rural, Guwahati Rural, Indore Rural, Kochi Rural, Kolkata Rural, Mumbai Rural, Mysore Rural, Nagpur Rural, Patna Rural, Pune Rural, Surat Rural, Vadodara Rural, Jaipur Rural, Ranchi, Nashik, Agra, Kozhikode, 
+            </p>
+    </div>
+   
+   </div>
+   <div>
+    <div class="left">
+        <p>PAYMENT OPTIONS:</p>
+    </div>
+    <div id="footer-down-imge">
+        <img src="https://png.pngtree.com/png-clipart/20210606/original/pngtree-cash-on-delivery-logo-design-with-illustation-png-image_6376011.jpg" alt="">
+        <img src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Paytm_Logo_%28standalone%29.svg/2560px-Paytm_Logo_%28standalone%29.svg.png" alt="">
+        <img src="https://www.telecomlead.com/wp-content/uploads/2016/08/MobiKwik-Logo.png" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/RuPay-Logo.png" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/American_Express_logo_%282018%29.svg/1200px-American_Express_logo_%282018%29.svg.png" alt="">
+    </div>
+   </div>
+</div>
+<div id="end">
+    <p>Copyright © 2021-2023 Supermarket Grocery Supplies Pvt Ltd</p>
+</div>`
+}
+export default footer
